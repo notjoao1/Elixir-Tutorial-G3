df0e5829624aeb9dd6635ffcd9558c68c72c5c69
notjoao1 <joao.dourado1@ua.pt>
2023-12-03 14:23:22 +0000

    exercises: fault tolerance exercise

---
+++ exercises/fault_tolerance/fault_tolerance.exs
@@ -0,0 +1,26 @@
+# Desafio: muda este código para que possa correr para sempre com recuperação automática no caso de falhas
+# Dica: https://elixirschool.com/en/lessons/intermediate/concurrency#process-linking-2
+
+defmodule Example do
+  # 50% chance to work, 50% chance to fail
+  def work do
+    work = :rand.uniform() > 0.5
+    case work do
+      true ->
+        IO.puts("working")
+        Process.sleep(200)
+        work()
+      false ->
+        exit(:cant_work)
+    end
+  end
+
+  def run do
+    spawn_link(Example, :work, [])
+  end
+end
+
+# Para correr (no iex):
+#
+#   Code.require_file("fault_tolerance.exs")
+#   Example.run()
